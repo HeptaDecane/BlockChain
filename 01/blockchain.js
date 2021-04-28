569fcfae309f9cbfb97b15dbf06eb9eb2537db66
HeptaDecane <sooraj1999vs@gmail.com>
2021-04-29 01:16:10 +0530

    Backend

--- 01/blockchain.js
+++ 01/blockchain.js
@@ -1,4 +1,5 @@
 const {Block} = require('./block')
+const {cryptoHash} = require('./crypto-hash')
 
 class Blockchain {
     constructor() {
@@ -12,6 +13,24 @@ class Blockchain {
         })
 
         this.chain.push(newBlock)
+    }
+
+    static isValidChain(chain){
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+            return false
+
+        for(let i=1; i<chain.length; i++){
+            const {timestamp, data, hash, lastHash} = chain[i]
+            const previousHash = chain[i-1].hash
+            if(lastHash !== previousHash)
+                return false
+
+            const reHash = cryptoHash(timestamp, data, lastHash)
+            if(hash !== reHash)
+                return false
+        }
+
+        return true
     }
 }
 
--- 01/blockchain.test.js
+++ 01/blockchain.test.js
@@ -2,7 +2,10 @@ const {Block} = require('./block')
 const {Blockchain} = require('./blockchain')
 
 describe('Blockchain', ()=>{
-    const blockchain = new Blockchain();
+    let blockchain
+    beforeEach(()=>{
+        blockchain = new Blockchain();
+    })
 
     it('contains a `chain` of Array instance', ()=>{
         expect(blockchain.chain instanceof Array).toBe(true)
@@ -19,6 +22,48 @@ describe('Blockchain', ()=>{
             // console.log(blockchain)
 
             expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(data)
+        })
+    })
+
+    describe('isValidChain()', ()=>{
+        beforeEach(()=>{
+            blockchain.addBlock({data: 'January'})
+            blockchain.addBlock({data: 'February'})
+            blockchain.addBlock({data: 'March'})
+            blockchain.addBlock({data: 'April'})
+        })
+
+        describe('when chain does not start with genesis block', ()=>{
+            it('returns false',()=>{
+                blockchain.chain[0] = {
+                    timestamp: Date.now(),
+                    data: "mod-data",
+                    hash: "mod-first-hash",
+                    lastHash: null
+                }
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false)
+            })
+        })
+
+        describe('when a `lastHash` reference is invalid', ()=>{
+            it('returns false', ()=>{
+                blockchain.chain[2].lastHash = 'mod-last-hash'
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false)
+            })
+        })
+
+        describe('when a chain contain blocks with invalid field', ()=>{
+            it('returns true', ()=>{
+                blockchain.chain[3].data = "mod-data"
+                blockchain.chain[3].timestamp = Date.now();
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false)
+            })
+        })
+
+        describe('when all blocks are valid', ()=>{
+            it('returns true', ()=>{
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(true)
+            })
         })
     })
 })
