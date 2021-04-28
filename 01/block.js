21edf92e1dbe1a15001a7340b2a758478dea834f
HeptaDecane <sooraj1999vs@gmail.com>
2021-04-28 14:53:20 +0530

    The Blocks | Backend

--- 01/block.js
+++ 01/block.js
@@ -1,9 +1,15 @@
+const {GENESIS_DATA} = require('./config')
+
 class Block {
     constructor({timestamp, data, hash, lastHash}) {
         this.timestamp = timestamp
         this.data = data
         this.hash = hash
         this.lastHash = lastHash
+    }
+
+    static genesis(){
+        return new Block(GENESIS_DATA)
     }
 }
 
---
+++ 01/config.js
@@ -0,0 +1,8 @@
+const GENESIS_DATA = {
+    timestamp: 0,
+    data: [],
+    hash: "first-hash",
+    lastHash: null
+}
+
+exports.GENESIS_DATA = GENESIS_DATA
