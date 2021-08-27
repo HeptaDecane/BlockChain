eb62bbe229521a2b62878db0da3053e8044ba58a
HeptaDecane <sooraj1999vs@gmail.com>
2021-08-27 23:27:57 +0530

    deploying to public testnet

--- solidity/inbox/tests/Inbox.test.js
+++ solidity/inbox/tests/Inbox.test.js
@@ -8,30 +8,31 @@ const web3 = new Web3(provider)
 
 let accounts
 let inbox
+let inboxTxn
 const initialMessage = "Hello"
 
 beforeEach(async ()=>{
     accounts = await web3.eth.getAccounts()
-    inbox = await new web3.eth.Contract(abi)
-        .deploy({data: bytecode, arguments:[initialMessage]})
+    inbox = new web3.eth.Contract(abi)
+    inboxTxn = await inbox.deploy({data: bytecode, arguments:[initialMessage]})
         .send({from: accounts[0], gas:1000000})
 })
 
 describe('Inbox',()=>{
     it('deploys a contract',()=>{
-        expect(inbox.options.address).toBeTruthy()
+        expect(inboxTxn.options.address).toBeTruthy()
     })
 
     it('has a default message',async ()=>{
-        let message = await inbox.methods.message().call()
+        let message = await inboxTxn.methods.message().call()
         expect(message).toEqual(initialMessage)
     })
 
     it('can update the message', async ()=>{
         let newMessage = "Bye"
-        await inbox.methods.setMessage(newMessage).send({from: accounts[0]})
+        await inboxTxn.methods.setMessage(newMessage).send({from: accounts[0]})
 
-        let message = await inbox.methods.message().call()
+        let message = await inboxTxn.methods.message().call()
         expect(message).toEqual(newMessage)
 
     })
