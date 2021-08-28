2720fb02f021df22324c9f2bec3bc12391aaa004
HeptaDecane <sooraj1999vs@gmail.com>
2021-08-28 13:57:15 +0530

    solidity mappingx

---
+++ solidity/Mapping.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+contract Mapping{
+    mapping(uint256=>string) map;
+    mapping(uint256=>bool) boolMap;
+    
+    function addOrUpdate(uint256 key, string memory value) public{
+        map[key] = value;
+        boolMap[key] = true;
+    }
+    
+    function get(uint256 key) public view returns(string memory){
+        return map[key];
+    }
+    
+    function contains(uint256 key) public view returns(bool){
+        return boolMap[key];
+    }
+    
+    function deleteEntry(uint256 key) public{
+        delete map[key];
+        boolMap[key] = false;
+    }
+
+}
